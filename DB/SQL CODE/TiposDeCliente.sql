addd4eef251d10a4efd7a8e52d158e5c0cfee2cb
Telphy <frazaokzn@hotmail.com>
2022-01-20 12:26:31 -0100

    1/20 (dentro de aula)

---
+++ DB/SQL CODE/TiposDeCliente.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mydb`.`tipoclientes` (`idTipoClientes`, `Tipo`) VALUES ('1', 'Cliente');
+INSERT INTO `mydb`.`tipoclientes` (`idTipoClientes`, `Tipo`) VALUES ('2', 'Novo Cliente');
+INSERT INTO `mydb`.`tipoclientes` (`idTipoClientes`, `Tipo`) VALUES ('3', 'Stand de Automoveis');
